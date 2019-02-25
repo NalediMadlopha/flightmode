eef1b44eee9a718f6f3e63888d86f15a6b63d861
Naledi Madlopha <naledi.madlopha@gmail.com>
2019-02-25 21:55:40 +0200

    refactor: Implement data models using the JsonToKotlinClass plugin

--- app/src/main/java/com/flightmode/app/model/FlightSchedule.kt
+++ app/src/main/java/com/flightmode/app/model/FlightSchedule.kt
@@ -1,9 +1,41 @@
 package com.flightmode.app.model
 
 data class FlightSchedule(
-    val airlineName: String,
+    val airline: Airline,
+    val arrival: Arrival,
+    val codeshared: Codeshared,
+    val departure: Departure,
+    val flight: Flight,
     val status: String,
-    val departureTime: String,
-    val flightNumber: String,
-    val destination: String
+    val type: String
+)
+
+data class Airline(
+    val iataCode: String,
+    val icaoCode: String,
+    val name: String
+)
+
+data class Arrival(
+    val iataCode: String,
+    val icaoCode: String,
+    val scheduledTime: String
+)
+
+data class Departure(
+    val iataCode: String,
+    val icaoCode: String,
+    val scheduledTime: String,
+    val terminal: String
+)
+
+data class Codeshared(
+    val airline: Airline,
+    val flight: Flight
+)
+
+data class Flight(
+    val iataNumber: String,
+    val icaoNumber: String,
+    val number: String
 )
