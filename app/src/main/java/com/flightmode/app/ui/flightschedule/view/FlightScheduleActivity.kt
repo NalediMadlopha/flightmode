99872ac6e5be51cb6d5b8b8e98a650b0361fcc1b
Naledi Madlopha <naledi.madlopha@gmail.com>
2019-02-25 09:55:19 +0200

    feature: Implement a master/detail flow

---
+++ app/src/main/java/com/flightmode/app/ui/flightschedule/view/FlightScheduleActivity.kt
@@ -0,0 +1,14 @@
+package com.flightmode.app.ui.flightschedule.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.flightmode.app.R
+
+class FlightScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.flight_schedule_activity)
+    }
+
+}
