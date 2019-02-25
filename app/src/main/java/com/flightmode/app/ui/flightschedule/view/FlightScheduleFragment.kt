0a4fd7dde7a48048e1b503f26a8fc100c96f6626
Naledi Madlopha <naledi.madlopha@gmail.com>
2019-02-25 14:19:58 +0200

    feature: Implement the flight schedule collapsing layout

--- app/src/main/java/com/flightmode/app/ui/flightschedule/view/FlightScheduleFragment.kt
+++ app/src/main/java/com/flightmode/app/ui/flightschedule/view/FlightScheduleFragment.kt
@@ -7,11 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.flightmode.app.R
+import kotlinx.android.synthetic.main.flight_schedule_fragment.*
+
 
 class FlightScheduleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.flight_schedule_fragment, container, false)
+        setupToolBar()
+        return inflater.inflate(com.flightmode.app.R.layout.flight_schedule_fragment, container, false)
     }
 
+    private fun setupToolBar() {
+        if (toolbar != null) {
+            toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+            toolbar.setNavigationOnClickListener { activity!!.onBackPressed() }
+        }
+    }
 }
